6ae2883dc8f5403ae5c545b4d4ce4a20adaaf200
geriiy <gheriyaj@gmail.com>
2025-02-17 11:18:10 +0700

    membuat view

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,10 +25,16 @@ Route::get('/hello', [WelcomeController::class, 'hello']);
 Route::get('/', HomeController::class);
 Route::get('/about', AboutController::class);
 Route::get('/articles/{id}', ArticleController::class);
+Route::resource('photos', PhotoController::class);
 
 Route::get('/world', function () {
     return 'World';
    });
+
+Route::get('/greeting', function () {
+    return view('hello', ['name' => 'Gheriya']);
+    });
+    
 
 
 
